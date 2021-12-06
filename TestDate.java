abab203ff4dc18de3ee5d74ceb233a77c88db6d7
Vianney Accart <vianneyaccart@gmail.com>
2021-12-06 12:05:13 +0100

    Resolve exercise

--- TestDate.java
+++ TestDate.java
@@ -17,7 +17,15 @@ class TestDate {
         int year = sc.nextInt();
         sc.close();
 
-        // TODO : your code after this line
-        
+        // Set the right format in the instance of SimpleDateFormat
+        SimpleDateFormat newDate = new SimpleDateFormat("MM-dd-yyyy");
+        // Use datas from Scanner in my instance of GregorianCalendar
+        Calendar calendar = new GregorianCalendar(year, month-1, day);
+        Date date = calendar.getTime();
+        // Get day of week from 1 (sunday) to 7 (saturday)
+        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+        // Print the result in the console
+        System.out.println("The date is : " + newDate.format(date));
+        System.out.println("The day of week is : " + (dayOfWeek));       
     }
 }
